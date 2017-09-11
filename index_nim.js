5fc4870a62a7cd177c87f80ca297a026f829b224
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-09-11 13:17:53 -0400

    changes made

--- index_nim.js
+++ index_nim.js
@@ -1,18 +1,44 @@
+var dotsBoard = 16;
+var numberOfDots = parseInt(prompt("How many do you wish to remove? Choose 1, 2 or 3."));
 
-
-
-var playerChoice = 2    // parseint(prompt("How many dots do you want to remove? No more than 3."));
-
-
-function removeDots(num) {
-  var numberOfDots = 16;
+//I need a function that gets player input. How many dots do they want to remove.
+function playerChoice() {
+  //var numberOfDots = parseInt(prompt("How many do you wish to remove? Choose 1, 2 or 3."));
   console.log(numberOfDots);
-  if (numberOfDots > 0) {
-    var numberOfDots = numberOfDots - playerChoice;
-    console.log(numberOfDots);
+}
+ playerChoice();
+
+// I need a function that switches players turns
+var playerOne = true; //Player two is false.
+function turnTracker() {
+  if(playerOne){
+    console.log("Go player one.")
+  } else {
+    console.log("Go player two")
   }
- //var numberOfDots = numberOfDots - playerChoice;
- //console.log(numberOfDots);
+  dotsRemaining();
+  playerOne = !playerOne;
 }
 
-removeDots(playerChoice);
+
+//I need a function that removes the number of dots given by the player.
+
+function removeDots(number) {
+  while (dotsStillOnBoard > 0) {
+    dotsStillOnBoard - playerChoice;
+  }
+}
+
+
+// I need to return the new number of dots after each player takes a turn.
+//Also return an error if player puts in a number of dots that can not be removed.
+ function dotsRemaining() {
+   var dotsStillOnBoard = dotsBoard - numberOfDots;
+   console.log(dotsStillOnBoard);
+ }
+dotsRemaining();
+ //I need to check if there is a winner
+
+ function didSomeoneWin() {
+
+ }
