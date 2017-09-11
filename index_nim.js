0a838cefd08e68392897d3143c4a74cb3f031bfc
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-09-11 11:08:48 -0400

    updated

--- index_nim.js
+++ index_nim.js
@@ -1,6 +1,18 @@
-var board = [
-  ["*", "*", "*", "*"];
-  ["*", "*", "*", "*"];
-  ["*", "*", "*", "*"];
-  ["*", "*", "*", "*"];
-];
+
+
+
+var playerChoice = 2    // parseint(prompt("How many dots do you want to remove? No more than 3."));
+
+
+function removeDots(num) {
+  var numberOfDots = 16;
+  console.log(numberOfDots);
+  if (numberOfDots > 0) {
+    var numberOfDots = numberOfDots - playerChoice;
+    console.log(numberOfDots);
+  }
+ //var numberOfDots = numberOfDots - playerChoice;
+ //console.log(numberOfDots);
+}
+
+removeDots(playerChoice);
